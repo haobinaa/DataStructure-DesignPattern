aadd83528c92fbddc5359bdc0ba74d46f9157ce8
haobin <815754194@qq.com>
2017-12-25 18:57:39 +0800

    插入排序修复

--- src/main/java/io/haobin/sort/SelectSort.java
+++ src/main/java/io/haobin/sort/SelectSort.java
@@ -9,21 +9,14 @@ import io.haobin.util.PrintUtil;
  */
 public class SelectSort {
     public static void selectSort(int[] table) {
+        int temp;
         for (int i = 0; i < table.length; i++) {
-            // 当前位置为最小值
-            int min = i;
             for(int j = i + 1; j < table.length; j++) {
-                // 如果小于当前最小值， 则min记录当前最小值
-                if(table[j] < table[min]) {
-                    min = j;
+                if(table[j] < table[i]) {
+                    temp = table[j];
+                    table[j] = table[i];
+                    table[i] = temp;
                 }
-                // 如果min不是当前刻度， 则把最小的那个放到当前位置，
-                if(min != i) {
-                    int temp = table[i];
-                    table[i] = table[j];
-                    table[j] = temp;
-                }
-                // 一趟完毕能找出此躺的最小值，下一次从刻度+1的位置开始找
             }
 
         }
---
+++ src/main/java/io/haobin/sort/ShellInsertSort.java
@@ -0,0 +1,10 @@
+package io.haobin.sort;
+
+/**
+ * 希尔排序
+ *
+ * @Author: HaoBin
+ * @Date 2017/12/25 18:57
+ */
+public class ShellInsertSort {
+}
