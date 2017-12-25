b3db8792d9b8160a68e772397a76c5d3d061ce3c
haobin <815754194@qq.com>
2017-12-25 23:11:28 +0800

    交换法希尔排序

--- src/main/java/io/haobin/sort/ShellInsertSort.java
+++ src/main/java/io/haobin/sort/ShellInsertSort.java
@@ -12,13 +12,13 @@ public class ShellInsertSort {
     // 插入法交换
     public static void shellSort(int[] table) {
         // 增量最开始等于length/2，并逐渐减少
-        for(int gap = table.length/2; gap > 0; gap /= 2) {
+        for (int gap = table.length / 2; gap > 0; gap /= 2) {
             // 从gap个元素，逐个对其所在组进行直接插入排序操作
-            for(int i = gap; i < table.length; i++) {
+            for (int i = gap; i < table.length; i++) {
                 int j = i;
                 int temp = table[j];
-                if(table[j] < table[j - gap]) {
-                    while(j - gap >= 0 && temp < table[j-gap]) {
+                if (table[j] < table[j - gap]) {
+                    while (j - gap >= 0 && temp < table[j - gap]) {
                         // 移动法
                         table[j] = table[j - gap];
                         j -= gap;
@@ -29,10 +29,35 @@ public class ShellInsertSort {
         }
     }
 
+    public static void insertShellSort(int[] table) {
+        for(int gap = table.length/2; gap > 0; gap /= 2) {
+            for (int i = gap; i < table.length; i++) {
+                int j = i;
+                while(j-gap >= 0 && table[j] < table[j-gap]) {
+                    // 插入排序采用交换法
+                    swap(table, j, j-gap);
+                    j -= gap;
+                }
+            }
+        }
+    }
+
+    /**
+     * 交换数组元素
+     * @param arr
+     * @param a
+     * @param b
+     */
+    public static void swap(int[] arr, int a, int b) {
+        arr[a] = arr[a] + arr[b];
+        arr[b] = arr[a] - arr[b];
+        arr[a] = arr[a] - arr[b];
+    }
+
     public static void main(String[] args) {
-        int[] intArr = {100,908,1000001,100,0,9999};
+        int[] intArr = {100, 908, 1000001, 100, 0, 9999};
         PrintUtil.printArray(intArr);
-        shellSort(intArr);
+        insertShellSort(intArr);
         System.out.println();
         PrintUtil.printArray(intArr);
     }
