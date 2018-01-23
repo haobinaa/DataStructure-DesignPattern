73db86db82fd5830da9aa36dc2dab51a0b16affc
haobin <815754194@qq.com>
2018-01-23 22:57:17 +0800

    实现arraylist

---
+++ src/main/java/io/haobin/collection/ArrayTest.java
@@ -0,0 +1,18 @@
+package io.haobin.collection;
+
+import io.haobin.collection.list.MyArrayList;
+
+/**
+ * @Author: HaoBin
+ * @Date 2018/1/11 9:52
+ */
+public class ArrayTest {
+    public static void main(String[] args) {
+        MyArrayList list = new MyArrayList();
+        list.add(1);
+        list.add(1);
+        list.add(1);
+        list.add(13);
+        list.add(1);
+    }
+}
