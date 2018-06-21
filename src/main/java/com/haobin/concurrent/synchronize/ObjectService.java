f130c3da595f54c6021fe0cad4f5ede494f0ffcf
haobin <815754194@qq.com>
2018-06-21 17:36:13 +0800

    类锁和对象锁

--- src/main/java/com/haobin/concurrent/synchronize/ObjectService.java
+++ src/main/java/com/haobin/concurrent/synchronize/ObjectService.java
@@ -4,21 +4,65 @@
 package com.haobin.concurrent.synchronize;
 
 /**
- *
- *
  * @author HaoBin
- * @version $Id: ObjectService.java, v0.1 2018/6/21 10:40 HaoBin 
+ * @version $Id: ObjectService.java, v0.1 2018/6/21 10:40 HaoBin
  */
 public class ObjectService {
+
     public void serviceMethod() {
         try {
             synchronized (this) {
-                System.out.println("begin time=" + System.currentTimeMillis());
-                Thread.sleep(2000);
-                System.out.println("end    end=" + System.currentTimeMillis());
+                System.out.println(Thread.currentThread().getName() + " begin time=" + System.currentTimeMillis());
+                Thread.sleep(1000);
+                System.out.println(Thread.currentThread().getName() + " end    end=" + System.currentTimeMillis());
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+}
+
+class ThreadAA extends Thread {
+
+    private ObjectService service;
+
+    public ThreadAA(ObjectService service) {
+        super();
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        service.serviceMethod();
+    }
+}
+
+class ThreadBB extends Thread {
+
+    private ObjectService service;
+
+    public ThreadBB(ObjectService service) {
+        super();
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        service.serviceMethod();
+    }
+}
+
+class ObjectServiceMain {
+
+    public static void main(String[] args) {
+        ObjectService service = new ObjectService();
+        ThreadAA a = new ThreadAA(service);
+        a.setName("a");
+        a.start();
+        ThreadBB b = new ThreadBB(service);
+        b.setName("b");
+        b.start();
     }
 }
---
+++ src/main/java/com/haobin/concurrent/synchronize/Service.java
@@ -0,0 +1,80 @@
+/**
+ * BrandBigData.com Inc. Copyright (c) 2018 All Rights Reserved.
+ */
+package com.haobin.concurrent.synchronize;
+
+/**
+ * @author HaoBin
+ * @version $Id: Service.java, v0.1 2018/6/21 11:48 HaoBin
+ */
+public class Service {
+
+    String anyString = new String();
+
+    public Service(String anyString) {
+        this.anyString = anyString;
+    }
+
+    public void setUsernamePassword(String username, String password) {
+        try {
+            synchronized (anyString) {
+                System.out.println("线程名称为：" + Thread.currentThread().getName()
+                        + "在" + System.currentTimeMillis() + "进入同步块");
+                Thread.sleep(3000);
+                System.out.println("线程名称为：" + Thread.currentThread().getName()
+                        + "在" + System.currentTimeMillis() + "离开同步块");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Thread1 extends Thread {
+
+    private Service service;
+
+    public Thread1(Service service) {
+        super();
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        service.setUsernamePassword("a", "aa");
+
+    }
+
+}
+
+class Thread2 extends Thread {
+
+    private Service service;
+
+    public Thread2(Service service) {
+        super();
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        service.setUsernamePassword("b", "bb");
+
+    }
+
+}
+
+class ServiceMain {
+
+    public static void main(String[] args) {
+        Service service = new Service("service string");
+
+        Thread1 a = new Thread1(service);
+        a.setName("A");
+        a.start();
+
+        Thread2 b = new Thread2(service);
+        b.setName("B");
+        b.start();
+    }
+}
