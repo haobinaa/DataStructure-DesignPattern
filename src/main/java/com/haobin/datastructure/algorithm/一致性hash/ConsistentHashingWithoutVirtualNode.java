acc9a1353c326eb20fabfcca3868ea6a83da7ea8
haobin <815754194@qq.com>
2018-08-16 16:21:51 +0800

    一致性hash，算法实现

---
+++ src/main/java/com/haobin/datastructure/algorithm/一致性hash/ConsistentHashingWithoutVirtualNode.java
@@ -0,0 +1,84 @@
+/**
+ * BrandBigData.com Inc. Copyright (c) 2018 All Rights Reserved.
+ */
+package com.haobin.datastructure.algorithm.一致性hash;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ * 不带虚拟节点的一致性hash
+ *
+ * @author HaoBin
+ * @version $Id: ConsistentHashingWithoutVirtualNode.java, v0.1 2018/8/16 10:42 HaoBin
+ */
+public class ConsistentHashingWithoutVirtualNode {
+
+    /**
+     * 待添加入Hash环的服务器列表
+     */
+    private static String[] servers = {"192.168.0.0:111", "192.168.0.1:111", "192.168.0.2:111",
+            "192.168.0.3:111", "192.168.0.4:111"};
+
+    /**
+     * key表示服务器的hash值，value表示服务器的名称
+     */
+    private static SortedMap<Integer, String> sortedMap =
+            new TreeMap<Integer, String>();
+
+    /**
+     * 程序初始化，将所有的服务器放入sortedMap中
+     */
+    static {
+        for (int i = 0; i < servers.length; i++) {
+            int hash = getHash(servers[i]);
+            System.out.println("[" + servers[i] + "]加入集合中, 其Hash值为" + hash);
+            sortedMap.put(hash, servers[i]);
+        }
+        System.out.println();
+    }
+
+    /**
+     * 使用FNV1_32_HASH算法计算服务器的Hash值,这里不使用重写hashCode的方法，最终效果没区别 36
+     */
+    private static int getHash(String str) {
+        final int p = 16777619;
+        int hash = (int) 2166136261L;
+        for (int i = 0; i < str.length(); i++) {
+            hash = (hash ^ str.charAt(i)) * p;
+        }
+        hash += hash << 13;
+        hash ^= hash >> 7;
+        hash += hash << 3;
+        hash ^= hash >> 17;
+        hash += hash << 5;
+        // 如果算出来的值为负数则取其绝对值
+        if (hash < 0) {
+            hash = Math.abs(hash);
+        }
+        return hash;
+    }
+
+    /**
+     * 得到应当路由到的结点
+     */
+    private static String getServer(String node) {
+        // 得到带路由的结点的Hash值
+        int hash = getHash(node);
+        // 得到大于该Hash值的所有Map
+        SortedMap<Integer, String> subMap =
+                sortedMap.tailMap(hash);
+        // 第一个Key就是顺时针过去离node最近的那个结点
+        Integer i = subMap.firstKey();
+        // 返回对应的服务器名称
+        return subMap.get(i);
+    }
+
+    public static void main(String[] args) {
+        String[] nodes = {"127.0.0.1:1111", "221.226.0.1:2222", "10.211.0.1:3333"};
+        for (int i = 0; i < nodes.length; i++) {
+            System.out.println("[" + nodes[i] + "]的hash值为" +
+                    getHash(nodes[i]) + ", 被路由到结点[" + getServer(nodes[i]) + "]");
+        }
+    }
+}
--- src/main/java/com/haobin/datastructure/algorithm/一致性hash/一致性hash.md
+++ src/main/java/com/haobin/datastructure/algorithm/一致性hash/一致性hash.md
@@ -10,9 +10,36 @@
 
 #### 2.一致性hash算法
 
-![]()
+1). 算法流程：
+
+先构造一个长度为232的整数环（这个环被称为一致性Hash环），根据节点名称的Hash值（其分布为[0, 2^32-1]）将缓存服务器节点放置在这个Hash环上，然后根据需要缓存的数据的Key值计算得到其Hash值（其分布也为[0, 
+2^32-1]），然后在Hash环上顺时针查找距离这个Key值的Hash值最近的服务器节点，完成Key到服务器的映射查找。
+
+如果在hash环上新增一个接口，影响的只有hash环上的一小段，并且整个集群下的节点越多，影响的范围越小
+
+![](https://raw.githubusercontent.com/haobinaa/DataStructure-DesignPattern/master/images/uniformity_hash.png)
 
 
+### 实现
+
+
+#### 1. 确定数据结构
+一致性hash算法的数据结构需要构造出一个2^32次方的整数环，我们需要一个尽可能低的时间复杂度的查找算法， 显然树形结构的时间复杂度更低， 所以一般选择 **二叉查找树**, 
+避免出现树退化成线性结构，不能直接使用二叉查找树，而是使用红黑树(或平衡二叉树AVL),JDK提供了红黑树的实现：TreeMap和TreeSet
+
+#### 2.Hash值的重新计算
+
+String的hashCode()方法的分散性很差，一段连续的ip分布区间比较固定(所有的节点会分布在一段很小的区间)，这样会导致绝大部分请求都落在第一个节点上(顺时针找到第一个节点)
+
+所以，String重写的hashCode()方法在一致性Hash算法中没有任何实用价值，得找个算法重新计算HashCode。这种重新计算Hash值的算法有很多，比如CRC32_HASH、FNV1_32_HASH、KETAMA_HASH等，其中KETAMA_HASH是默认的MemCache推荐的一致性Hash算法，用别的Hash算法也可以，比如FNV1_32_HASH算法的计算效率就会高一些。
+
+#### 3. 不带虚拟节点的实现
+
+使用FNV1_32_HASH来实现hash
+
+- [不带虚拟节点的实现]()
+
+#### 4. 使用虚拟节点来改善一致性Hash算法
 
 ### 参考资料
 - [memcache详细解读](http://www.cnblogs.com/xrq730/p/4948707.html)
