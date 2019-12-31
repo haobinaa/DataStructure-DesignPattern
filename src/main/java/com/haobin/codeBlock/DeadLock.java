958bc0ed61eb5eab36f0de850ef88a2326186399
haobin <815754194@qq.com>
2019-12-31 11:19:36 +0800

    预防死锁和消除死锁

--- src/main/java/com/haobin/codeBlock/DeadLock.java
+++ src/main/java/com/haobin/codeBlock/DeadLock.java
@@ -8,7 +8,15 @@ package com.haobin.codeBlock;
  * 两个线程持有一个条件，竞争另一个条件形成死锁
  *
  * @author HaoBin
- * @version $Id: DeadLock.java, v0.1 2018/10/31 13:48 HaoBin 
+ * @version $Id: DeadLock.java, v0.1 2018/10/31 13:48 HaoBin
+ *
+ * 打破死锁产生的四个必要条件之一(请求互斥，请求保持，不可剥夺，循环等待)死锁就不会发生
+ *
+ *
+ * 当死锁发生了，如果要消除死锁，有如下几种方式:
+ * 1. 最简单的就是重启，不过这种方式代价较大，会导致之前所有的状态都被清除
+ * 2. 撤销进程，剥夺资源。终止参与死锁的进程，收回它们占用的资源。
+ * 3. 进程回退，如果发生死锁则退到上一步
  */
 public class DeadLock extends Thread{
 
