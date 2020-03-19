a9c679427dc2942b91a6d8ffdf5a337fb119335a
haobin <815754194@qq.com>
2020-03-19 18:02:18 +0800

    redis 分布式锁集群版本

--- src/main/java/com/haobin/codeBlock/lock/ClusterRedisDistributeLock.java
+++ src/main/java/com/haobin/codeBlock/lock/ClusterRedisDistributeLock.java
@@ -4,6 +4,8 @@ package com.haobin.codeBlock.lock;
  * @Author HaoBin
  * @Create 2020/3/17 17:59
  * @Description: 集群下分布式锁
+ *
+ * redisson 对 redlock 的实现： https://www.cnblogs.com/zhili/p/redLock_DistributedLock.html
  **/
 public class ClusterRedisDistributeLock {
 
